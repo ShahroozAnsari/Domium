1e48d1e0ac82dd75d841a3d1368c3ef9028a181c
shahrooz <sh.ansari2014.com>
2026-07-09 01:10:45 +0330

    maked entitybase simple

--- Domain/Domium.Domain/EntityBase.cs
+++ Domain/Domium.Domain/EntityBase.cs
@@ -1,6 +1,4 @@
 using Domium.Domain.Abstractions.Entity;
-using Domium.Domain.Abstractions.Aggregate;
-using System.Runtime.CompilerServices;
 
 namespace Domium.Domain;
 
@@ -27,33 +25,22 @@ public abstract class EntityBase<TId> : EntityBase, IEntityBase<TId>
 
     public override bool Equals(object? obj)
     {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
         if (obj is not EntityBase<TId> other)
         {
             return false;
         }
 
-        if (ReferenceEquals(this, other))
-        {
-            return true;
-        }
-
-        if (HasDefaultIdentity(Id) || HasDefaultIdentity(other.Id))
-        {
-            return false;
-        }
-
-        return GetType() == other.GetType() && EqualityComparer<TId>.Default.Equals(Id, other.Id);
+        return GetType() == other.GetType() &&
+               EqualityComparer<TId>.Default.Equals(Id, other.Id);
     }
 
-    public override int GetHashCode()
-    {
-        if (HasDefaultIdentity(Id))
-        {
-            return RuntimeHelpers.GetHashCode(this);
-        }
-
-        return HashCode.Combine(GetType(), Id);
-    }
+    public override int GetHashCode() =>
+        HashCode.Combine(GetType(), Id);
 
     public static bool operator ==(EntityBase<TId>? left, EntityBase<TId>? right)
     {
@@ -63,30 +50,5 @@ public abstract class EntityBase<TId> : EntityBase, IEntityBase<TId>
     public static bool operator !=(EntityBase<TId>? left, EntityBase<TId>? right)
     {
         return !Equals(left, right);
-    }
-
-    private static bool HasDefaultIdentity(TId id)
-    {
-        if (id is not IAggregateId aggregateId)
-        {
-            return EqualityComparer<TId>.Default.Equals(id, default!);
-        }
-
-        var value = aggregateId.Value;
-
-        if (value is null)
-        {
-            return true;
-        }
-
-        if (value is string text)
-        {
-            return string.IsNullOrWhiteSpace(text);
-        }
-
-        var valueType = value.GetType();
-
-        return valueType.IsValueType &&
-               value.Equals(Activator.CreateInstance(valueType));
     }
 }
