54513b1f87eaedaef3b2332cbcc40ec290c19a87
Shahrooz <Ansari.sh@toptec.ir>
2026-07-18 11:23:11 +0330

    add job filters

--- Configuration/Domium.Configuration/Jobs/JobOptions.cs
+++ Configuration/Domium.Configuration/Jobs/JobOptions.cs
@@ -7,4 +7,8 @@ public sealed class JobOptions
     public string CronExpression { get; set; } = string.Empty;
 
     public bool Enabled { get; set; } = true;
+
+    public int RetryAttempts { get; set; }
+
+    public bool DisableConcurrentExecution { get; set; }
 }
